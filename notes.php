35975f0d3856db05faaefe15111dae89d31b35d2
thinesh1 <thinesh@rhino-partners.com>
2021-07-25 16:13:37 +0530

    all commit

--- notes.php
+++ notes.php
@@ -1,4 +1,5 @@
 # composer create-project laravel/laravel your-project-name 
 # php artisan serve 
 
-# 
+# php artisan route:list
+# php artisan make:controller controller_name --invokable (with invoke method) 
